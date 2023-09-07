688a7c9068fcaf3e5c0b8d10d6a56ab84476379f
Ikram Akbar <ikramakbar71.com>
2023-09-07 22:16:35 +0600

    init the project with -react router dom, tailwind, daisy ui

---
+++ src/Router/Routes.jsx
@@ -0,0 +1,27 @@
+import {createBrowserRouter} from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+import NotFound from "../Pages/NotFound/NotFound";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+            {
+                path: "home",
+                element: <Home></Home>,  
+            },
+            {
+                path:"*",
+                element:<NotFound></NotFound>
+            }
+        ]
+    },
+]);
+export default router;
